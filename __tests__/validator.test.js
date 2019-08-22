31dfcf5d6fb0005acf8d7c06a31ddb9012b6b53c
Bradley Elliott <bgelliott206@gmail.com>
2019-08-21 22:20:19 -0700

    isGreaterThanFour test and validator added

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -3,16 +3,37 @@
 const validator = require('../lib/validator');
 
 describe('#validator', () => {
-    describe('#isLessThanTen', () => {
-        test ('regular cases', () => {
-            expect(validator.isLessThanTen(input,5)).toEqual(true);
-            expect(validator.isLessThanTen(input,11)).toEqual(false);
-            expect(validator.isLessThanTen(input,10)).toEqual(false);
-            expect(validator.isLessThanTen(input,-1)).toEqual(true);
+    describe('#isGreaterThanFour', () => {
+        test ('all cases', () => {
+            expect(validator.isGreaterThanFour(input, 5)).toEqual(true);
+            expect(validator.isGreaterThanFour(input, 2)).toEqual(false);
+            expect(validator.isGreaterThanFour(input, 10)).toEqual(true);
+            expect(validator.isGreaterThanFour(input, -1)).toEqual(false);
         });
     });
 });
 
+
+
 // function
+// const getMinMax = require('../get-biggest');
+
+// describe('Find the biggest number' () => {
+
+//     test('Test normal input', () => {
+
+//         expect(getMinMax.getBiggest(1,2,3)).toEqual(3);
+        
+//         expect(getMinMax.getBiggest(10,2,3)).toEqual(10);
+//     });
+
+//     test('Test equal input', () => {
+//         expect(getMinMax.getBiggest(2,2,2)).toEqual(2);
+//     });
+
+//     test('Test negative input', () => {
+//         expect(getMinMax.getBiggest(-4, -3, -10)).toEqual(-3)
+//     });
+// });
 
 // object 
